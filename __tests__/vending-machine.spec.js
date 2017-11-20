09068a47873c44c5f952b4d1df0aad21f1c01ae7
corawym <corawym@gmail.com>
2017-11-20 00:03:42 -0800

    Add printing inventory function

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -2,98 +2,108 @@ const vendingMachine = require('../src/vending-machine.js');
 
 describe('vendingMachine', () => {
 
+  beforeEach(() => {
+    test.subject = new vendingMachine();
+  })
 
+  
   describe('Printing inventory', () => {
 
     describe('When a given inventory exists', () => {
       it('should return the correct inventory name, price and quantity', () => {
-        expect().toBe();
+        const result = test.subject.printInventory('Water');
+        const expectedResult = [
+          {
+            "item": "Water",
+            "price": 2,
+            "maxQuantity": 10,
+            "quantity": 10
+          }
+        ]
+        expect(result).toEqual(expectedResult);
       });
     });
 
     describe('When a given inventory does not exist', () => {
       it('should throw an error', () => {
-        expect().toThrow();
+        expect(() => test.subject.printInventory('orange')).toThrow('Please enter a valid item name');
       });
     });
 
   })
 
 
-  describe('Refilling inventory', () => { 
+  // describe('Refilling inventory', () => { 
 
-    describe('When a given inventory is refilled', () => {
-      it('should return the correct inventory name and quantity', () => {
-        expect().toBe();
-      });
-    });
+  //   describe('When a given inventory is refilled', () => {
+  //     it('should return the correct inventory name and quantity', () => {
+  //       expect().toBe();
+  //     });
+  //   });
 
-    describe('When a given inventory is full', () => {
-      it('should return it is full', () => {
-        expect().toBe();
-      });
-    });
+  //   describe('When a given inventory is full', () => {
+  //     it('should return it is full', () => {
+  //       expect().toBe();
+  //     });
+  //   });
 
-  })
+  // })
 
   
-  describe('Re-supplying change', () => { 
+  // describe('Re-supplying change', () => { 
 
-    describe('When a given denomination is resupplied', () => {
-      it('should return the correct denomination, value and quantity', () => {
-        expect().toBe();
-      });
-    });
+  //   describe('When a given denomination is resupplied', () => {
+  //     it('should return the correct denomination, value and quantity', () => {
+  //       expect().toBe();
+  //     });
+  //   });
 
-    describe('When a given denomination is full', () => {
-      it('should return it is full', () => {
-        expect().toBe();
-      });
-    });
+  //   describe('When a given denomination is full', () => {
+  //     it('should return it is full', () => {
+  //       expect().toBe();
+  //     });
+  //   });
 
-  })
-
-
-  describe('Dispensing inventory based on payment', () => {
-
-    describe('When a given payment is 0', () => {
-      it('should not return any inventories', () => {
-        expect().toBe();
-      });
-    });
-
-    describe('When a given payment is 10', () => {
-      it('should return the affordable and available inventories', () => {
-        expect().toBe();
-      });
-    });
-
-  })
+  // })
 
 
-  describe('Purchasing inventories', () => {
+  // describe('Dispensing inventory based on payment', () => {
 
-    describe('When a given payment exists and inventories are selected', () => {
-      it('should return the correct items with new quantity', () => {
-        expect().toBe();
-      });
-    });
+  //   describe('When a given payment is 0', () => {
+  //     it('should not return any inventories', () => {
+  //       expect().toBe();
+  //     });
+  //   });
 
-  })
+  //   describe('When a given payment is 10', () => {
+  //     it('should return the affordable and available inventories', () => {
+  //       expect().toBe();
+  //     });
+  //   });
 
-
-  describe('Returning change as coins', () => {
-
-    describe('When a change is required', () => {
-      it('should return the correct denominations and value', () => {
-        expect().toBe();
-      });
-    });
-
-  })
+  // })
 
 
+  // describe('Purchasing inventories', () => {
 
+  //   describe('When a given payment exists and inventories are selected', () => {
+  //     it('should return the correct items with new quantity', () => {
+  //       expect().toBe();
+  //     });
+  //   });
+
+  // })
+
+
+  // describe('Returning change as coins', () => {
+
+  //   describe('When a change is required', () => {
+  //     it('should return the correct denominations and value', () => {
+  //       expect().toBe();
+  //     });
+  //   });
+
+  // })
 
 
 });
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,28 +1,107 @@
+const data = {
+  "inventories": [
+    {
+      "item": "Water",
+      "price": 2,
+      "maxQuantity": 10,
+      "quantity": 10
+    },
+    {
+      "item": "Coke",
+      "price": 2,
+      "maxQuantity": 10,
+      "quantity": 2
+    },
+    {
+      "item": "Juice",
+      "price": 3,
+      "maxQuantity": 10,
+      "quantity": 5
+    },
+    {
+      "item": "Chips",
+      "price": 2.75,
+      "maxQuantity": 10,
+      "quantity": 0
+    },
+    {
+      "item": "Chocolate",
+      "price": 2.50,
+      "maxQuantity": 10,
+      "quantity": 5
+    },
+  ],
+  "change": [
+    {
+      "denomination": "nickel",
+      "value": 0.05,
+      "maxQuantity": 10,
+      "quantity": 5
+    },
+    {
+      "denomination": "dime",
+      "value": 0.10,
+      "maxQuantity": 10,
+      "quantity": 5
+    },
+    {
+      "denomination": "quarter",
+      "value": 0.25,
+      "maxQuantity": 10,
+      "quantity": 5
+    },
+    {
+      "denomination": "loonie",
+      "value": 1.00,
+      "maxQuantity": 10,
+      "quantity": 10
+    },
+    {
+      "denomination": "toonie",
+      "value": 2.00,
+      "maxQuantity": 10,
+      "quantity": 10
+    }
+  ]
+}
+
 class vendingMachine {
 
   constructor(){
-    
+    this.inventories = data.inventories,
+    this.change = data.change
   }
 
-  printInventory(){
-
+  printInventory(itemName){
+    if (typeof itemName === 'string' && itemName.length > 0) {
+      const itemFound = this.inventories.filter((inventory) => {
+        return itemName === inventory.item
+      })
+      // console.log(itemFound)
+      if (itemFound.length > 0) {
+        return itemFound
+      } else {
+        throw new Error('Please enter a valid item name')
+      }
+    }
+    throw new Error('Please enter a valid item name')
   }
 
-  refillInventory(){
+  // refillInventory(){
 
-  }
+  // }
 
-  resupplyChange(){
+  // resupplyChange(){
 
-  }
+  // }
 
-  dispenseInventory(){
+  // dispenseInventory(){
 
-  }
+  // }
 
-  returnChange(){
+  // returnChange(){
 
-  }
+  // }
 
 }
 
